f1f9ab49003c1e60838e9b0005fd2fe9200278f0
NegohPreto <jgabrieltomaz2706@gmail.com>
2019-08-28 17:27:02 -0300

    Lista de criptomoedas

--- js/main.js
+++ js/main.js
@@ -10,26 +10,31 @@ function animabtn(){
 function carregaJson(){
     endpoint = 'live';
     access_key = '439ae63d8067273e1293e14b1012db29';
-    target = 'BRL';
     $.ajax({
         url: 'http://api.coinlayer.com/api/' + endpoint + '?access_key=' + access_key,   
         dataType: 'jsonp',
-        success: function(json) {
-
-
+        success: function(date) {
+            
+            console.log(date.rates);
+            var moedas= date.rates;
             const resultado = document.getElementById('moeda');
 
             const tabelaResultado = resultado.getElementsByTagName('table')[0];
     
-            if (tabelaResultado == undefined) {
-                resultado.innerHTML = '<table class="result"><thead><td>Moeda</td><td>Preço</td></thead><tbody></tbody></table>';
-            }
             
             let tabelaResultadoBody = resultado.getElementsByTagName('tbody')[0];
-            tabelaResultadoBody.innerHTML += `<tr>
-            <td>${json.rates.BTC}</td>
-            <td>${json.target}</td>
-        </tr>`;
+            var coluna1 = '';
+            var i;
+            for(prop in moedas){
+                console.log(prop);
+                coluna1 += "<tr><td>"+ prop+"</td>";
+                coluna1 += "<td>"+ moedas[prop]+"</td></tr>";
+                
+            }
+            // for(i = 0; i < moedas.length;i++){
+            //     tabela += "<td>"+ moedas[i]+"</td>"
+            // }
+            tabelaResultadoBody.innerHTML = coluna1;
         }
     });
 }
