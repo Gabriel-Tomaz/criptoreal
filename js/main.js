caf73b14dbe4ce79455ecc802993a428e8e9470b
NegohPreto <jgabrieltomaz2706@gmail.com>
2019-09-08 03:16:56 -0300

    Pesquisa de moedas por data e escolha de quantidade de moedas a ser listado.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,15 @@ function animabtn(){
         navbar.classList.toggle('change');
     });
 }
-
+function quantidade_moedas(){
+ 
+}
 function carregaJson(){
-    endpoint = 'live';
-    access_key = '439ae63d8067273e1293e14b1012db29';
     $.ajax({
         url: 'http://api.coinlayer.com/api/live?access_key=439ae63d8067273e1293e14b1012db29&target=BRL',   
         dataType: 'jsonp',
         success: function(date) {
             
-            console.log(date.rates);
             var moedas= date.rates;
             const resultado = document.getElementById('moeda');
 
@@ -30,9 +29,10 @@ function carregaJson(){
                 cripto.push(moeda);
             }
             cripto = insertion_Sort(cripto);
-            console.log(cripto);
-            
-            for(i = 0; i < 10;i++){
+            let quant = document.getElementById('n-moedas').value;
+            let converte = parseInt(quant);
+            converte = converte;
+            for(i = 0; i < converte;i++){
                 var moeda = cripto[i];
                 coluna1 += "<tr><td>"+ moeda[0]+"</td>";
                 coluna1 += "<td>"+"R$ "+ moeda[1]+"</td></tr>";
@@ -41,8 +41,42 @@ function carregaJson(){
         }
     });
 }
+function carregaJsonDatas(){
+  let valorData = document.getElementById('n-datas').value;
+  if(valorData == ""){
+    alert("O campo de data não pode está vazio!");
+  }
+  $.ajax({
+      url: 'http://api.coinlayer.com/api/'+valorData+'?access_key=439ae63d8067273e1293e14b1012db29&target=BRL',   
+      dataType: 'jsonp',
+      success: function(date) {
+          
+          var moedas= date.rates;
+          const resultado = document.getElementById('moeda');
 
-
+          const tabelaResultado = resultado.getElementsByTagName('table')[0];
+  
+          
+          let tabelaResultadoBody = resultado.getElementsByTagName('tbody')[0];
+          var coluna1 = '';
+          var cripto = [];
+          for(prop in moedas){
+              moeda = [prop, moedas[prop]];
+              cripto.push(moeda);
+          }
+          cripto = insertion_Sort(cripto);
+          let quant = document.getElementById('n-moedas-d').value;
+          let converte = parseInt(quant);
+          converte = converte;
+          for(i = 0; i < converte;i++){
+              var moeda = cripto[i];
+              coluna1 += "<tr><td>"+ moeda[0]+"</td>";
+              coluna1 += "<td>"+"R$ "+ moeda[1]+"</td></tr>";
+          }
+          tabelaResultadoBody.innerHTML = coluna1;
+      }
+  });
+}
 function insertion_Sort(arr){
     for (var i = 1; i < arr.length; i++){
       if (arr[i][1] > arr[0][1]){
